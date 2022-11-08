62760c731c38e8e5dd603b23ad40f551453b87ae
Deekshith <20eg106205@anurag.edu.in>
2022-11-08 23:06:19 +0530

    vaidation check

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const {check, validationResult } = require('express-validator');
 const User = require('../../models/User');
 
 router.get('/' , (req, res) => res.send('User route'));
-router.post('/',async (req, res) => {
+router.post('/', 
+    [
+        check('name','Name is required').not().isEmail(),
+        check('email','Valid email address required').isEmail(),
+        check('password','password must have at least 4 characters').isLength({ min: 4 })
+    ],
+    async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
     const { name, email, password} = req.body;
     try {
         let user = await User.findOne({ email });
